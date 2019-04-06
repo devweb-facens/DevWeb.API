e63701de97895aec45d6526ecbc844ccc79f6929
Gabriel Yudi <g.sanefugi@gmail.com>
2019-04-06 16:10:15 -0300

    Update ProfessorRepository.java

--- DevWeb/src/main/java/br/com/devweb/repository/ProfessorRepository.java
+++ DevWeb/src/main/java/br/com/devweb/repository/ProfessorRepository.java
@@ -2,8 +2,8 @@ package br.com.devweb.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import br.com.devweb.models.Diciplina;
+import br.com.devweb.models.Professor;
 
-public interface ProfessorRepository extends CrudRepository<Diciplina, Integer>{
+public interface ProfessorRepository extends CrudRepository<Professor, Integer>{
 
 }
