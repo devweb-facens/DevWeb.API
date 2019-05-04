462e4e0cab9b2b118e2206f8d960bcf944c793ef
Dino <papaleguas95.19@gmail.com>
2019-05-04 14:11:45 -0300

    implementacao de delete

--- DevWeb/src/main/java/br/com/devweb/controller/AdminController.java
+++ DevWeb/src/main/java/br/com/devweb/controller/AdminController.java
@@ -13,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents;
 
 import br.com.devweb.models.Admin;
-import br.com.devweb.repository.AdminRepository;
 import br.com.devweb.services.AdminService;
 
 @RestController
